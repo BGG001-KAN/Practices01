2027b0f33df8372f793cd881915c391959e49741
bgg <bgg@softwareag.com>
2024-06-23 22:39:52 +0530

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String a[]){
+     int b=5;
+     String name="bharath";
+     float h=5.6f;
+     long y= 584562l;
+     boolean result= false;
+     int value= name.indexOf("r");
+    if(true){
+       result=true;
+      System.out.println(result);
+    }
+     System.out.println(y);
+    }
+}
